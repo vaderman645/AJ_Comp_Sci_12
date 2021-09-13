c97cd7fd4ca0ffd46ff10c7ecac10384da4bab0a
ajhen <hendaj746@gotvdsb.ca>
2021-09-13 10:46:13 -0400

    Exercise 3

---
+++ src/OnePointOneE3.java
@@ -0,0 +1,8 @@
+public class OnePointOneE3 {
+    public static void main(String[] args) {
+        System.out.println("Inspirational Quotes: ");
+        System.out.println("\n \"Great, kid, don't get cocky.\" —Han Solo, A New Hope.");
+        System.out.println("\n \"Try not. Do or do not. There is no try.\" —Yoda, Star Wars Episode V: The Empire Strikes Back");
+        System.out.println("\n \"Your eyes can deceive you; don’t trust them.\" —Obi-Wan Kenobi, Star Wars Episode IV: A New Hope");
+    }
+}
