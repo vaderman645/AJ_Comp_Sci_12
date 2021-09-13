ea61bd4e106d1de72fe33ad6474ca89dd65b1267
ajhen <hendaj746@gotvdsb.ca>
2021-09-13 11:30:08 -0400

    1.2

---
+++ src/OnePointTwo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class OnePointTwo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+System.out.println("Enter Type of Animal: ");
+String type = input.nextLine();
+System.out.println("Enter Amount of Animals: ");
+int amount = input.nextInt();
+System.out.println("Enter Projection Year: ");
+int year = input.nextInt();
+int maths = amount*(1 + (year - 2021));
+System.out.println("\n Type of Animal: " + type + "\n Starting Amount: " + amount + "\n Current Year: 2021 \n Projection Year: " + year + "\n Projected Population: " + maths);
+    }
+}
