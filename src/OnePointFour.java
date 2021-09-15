e54fdfe9b4a645a78f3614f3f375363aae06b092
ajhen <hendaj746@gotvdsb.ca>
2021-09-15 08:36:48 -0400

    1.4 challenge

--- src/OnePointFour.java
+++ src/OnePointFour.java
@@ -9,10 +9,11 @@ public class OnePointFour {
     String two = input.nextLine();
     System.out.println("Enter a verb: ");
     String three = input.nextLine();
-    String userinput = "1 was an interesting 2, but a unique 2. They could 3 better than any other 2. 3ing is 1's favourite activity";
+    String userinput = "1 was an interesting 2, but a unique 2. They could 3 better than any other 2. 4ing is 1's favourite activity";
     String firstreplace = userinput.replace("1",one);
     String secondreplace = firstreplace.replace("2",two);
-    String finalreplace = secondreplace.replace("3",three);
+    String thirdreplace = secondreplace.replace("3",three);
+    String finalreplace = thirdreplace.replace("4",three.substring(0,1).toUpperCase()+three.substring(1)); //challenge
     System.out.println(finalreplace);
 
     }
