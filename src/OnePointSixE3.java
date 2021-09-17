18c1ed2a62cbbe448da8c318f09a956a254a948b
ajhen <hendaj746@gotvdsb.ca>
2021-09-17 11:37:58 -0400

    1.6 E3

---
+++ src/OnePointSixE3.java
@@ -0,0 +1,10 @@
+public class OnePointSixE3 {
+    public static void main(String[] args) {
+        int a =0;
+int i = 0;
+int b = 0;
+        while (a<6){a++;System.out.print("\n");
+             for (int y=0; y<a;y++){System.out.print("*");}
+        }
+    }
+}
