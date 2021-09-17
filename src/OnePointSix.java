98d2c445b6e32dad06fcf075e2abcebcd9b1d021
ajhen <hendaj746@gotvdsb.ca>
2021-09-17 09:52:44 -0400

    1.6 E1

---
+++ src/OnePointSix.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OnePointSix {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Couning up by 3: \n");
+    for (int i = 0; i<22;i=i + 3){System.out.println(i);}
+System.out.println("\nCounting Down from 21: \n");
+    for (int i = 22;i>0;i=i){
+        if (i==20){i=1;}i = i -1;
+        System.out.println(i);
+    }
+
+    }
+}
