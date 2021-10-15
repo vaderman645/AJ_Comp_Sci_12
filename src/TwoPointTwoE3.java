a4ac25c592a5928dcc671015b8781e2bffc22b7a
ajhen <hendaj746@gotvdsb.ca>
2021-10-15 11:09:25 -0400

    2.2E3

--- src/TwoPointTwoE3.java
+++ src/TwoPointTwoE3.java
@@ -1,12 +1,17 @@
 public class TwoPointTwoE3 {
     public static void main(String[] args) {
-        char arr[][] = new char[5][];
-        int length [] = {5,8,11,8,8};
-        String name []= {"Turing","Schwartz","Burners-Lee","Lovelace","Torvalds"};
+        char[][] arr = new char[5][];
+        int[] length = {5,8,11,8,8};
+        String[] name = {"Turing","Schwartz","Burners-Lee","Lovelace","Torvalds"};
         for (int i = 0;i<4;i++){arr[i]=new char[length[i]];}
         for (int i = 0;i<5;i++){arr[i] = name[i].toCharArray();}
 
-        for (int i = 0;i<name[i].length();i++){System.out.println(arr[0][i]);}
+        for (int i = 0;i<5;i++){
+            for (int x = 0;x<arr[i].length;x++){
+                System.out.print(arr[i][x]);
+            }
+            System.out.println();
+        }
 
 
     }
