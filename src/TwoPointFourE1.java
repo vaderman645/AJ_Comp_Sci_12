4f206e7c09d36f4fb9d8553869baa5439f1f27db
vaderman645 <71646638+vaderman645@users.noreply.github.com>
2021-10-24 13:40:45 -0400

    2.4E1

--- src/TwoPointFourE1.java
+++ src/TwoPointFourE1.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class TwoPointFourE1 {
     static String RockPaper (String playerInt, String oppInp){
-        if (playerInt == oppInp){return "Tie!";}
-        else if (playerInt=="rock"&&oppInp=="scissors"){
-            return"Player Wins!";
-        }
+        if (playerInt.equalsIgnoreCase(oppInp)){return "Tie!";}
+        else if (playerInt.equalsIgnoreCase("rock")&&oppInp.equalsIgnoreCase("scissors")){return"Player Wins!";}
+        else if (playerInt.equalsIgnoreCase("rock")&&oppInp.equalsIgnoreCase("paper")){return"Player Loses!";}
+        else if (playerInt.equalsIgnoreCase("scissors")&&oppInp.equalsIgnoreCase("rock")){return"Player Loses!";}
+        else if (playerInt.equalsIgnoreCase("scissors")&&oppInp.equalsIgnoreCase("paper")){return"Player Wins!";}
+        else if (playerInt.equalsIgnoreCase("paper")&&oppInp.equalsIgnoreCase("scissors")){return"Player Loses!";}
+        else if (playerInt.equalsIgnoreCase("paper")&&oppInp.equalsIgnoreCase("rock")){return"Player Wins!";}
         else {return"";}
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter Your Input (Rock, Paper, Scissors");
+        System.out.println("Enter Your Input (Rock, Paper, Scissors)");
         String playerInp = input.nextLine();
         System.out.println("Enter Opponent Input");
         String oppInp = input.nextLine();
-        System.out.println();
+        System.out.println(RockPaper(playerInp,oppInp));
     }
 }
