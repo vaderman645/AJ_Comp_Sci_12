7f837efe721aac50e4d8c3cd5d95ffff8b8eb30b
vaderman645 <71646638+vaderman645@users.noreply.github.com>
2021-10-23 22:26:09 -0400

    2.3E3

--- src/TwoPointThreeE3.java
+++ src/TwoPointThreeE3.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
-
 public class TwoPointThreeE3 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter character: ");
-        char inp = Character.toUpperCase(input.next().charAt(0));
+    static int result (char inp){
         int count = 0;
         int result = 0;
         for (char inpCount = inp; inpCount != '['; inpCount++, count++) {
             result = 26 - count;
         }
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter character: ");
+        char inp = Character.toUpperCase(input.next().charAt(0));
+
+        System.out.println(result(inp));
     }
 }
