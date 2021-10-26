a7474f7e9d701c82e30db4117f09c6b2fe5467d1
ajhen <hendaj746@gotvdsb.ca>
2021-10-26 10:50:47 -0400

    2.5

--- src/TwoPointFive.java
+++ src/TwoPointFive.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class TwoPointFive {
+    static int Overload(int inp1,int inp2){
+        return inp1+inp2;
+    }
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    System.out.println("Enter Your Integers to add: ");
+    int intInp1 = input.nextInt();
+    int intInp2 = input.nextInt();
 
     }
 }
