a779e8aef4ab943394346d4644a95f97910d5385
ajhen <hendaj746@gotvdsb.ca>
2021-12-08 10:09:13 -0500

    3.5

---
+++ src/ThreePointFive1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ThreePointFive1 {
+    static int output=1;
+    static int exponents(int base, int exponent) {
+        if (exponent == 0) {
+            return output;
+        }
+        output = output * base;
+        return (exponents(base, exponent - 1));
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your base: ");
+        int base = input.nextInt();
+        System.out.println("Enter your exponent: ");
+        int exponent = input.nextInt();
+        System.out.println(exponents(base, exponent));
+    }
+}
---
+++ src/ThreePointFive2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ThreePointFive2 {
+    static int output = 1;
+    static int factor(int inp) {
+        if(inp ==0){return output;}
+        output = output * inp;
+        return factor(inp-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number to get the factorial of: ");
+        System.out.println(factor(input.nextInt()));
+    }
+}
